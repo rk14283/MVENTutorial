00d75f5570f955c43e4a14b8f8704d42b0c60c43
rk14283 <rk14283@my.bristol.ac.uk>
2023-08-01 22:13:35 +0100

    more on the MVEN APP

--- backend/routes/Todos.js
+++ backend/routes/Todos.js
@@ -34,11 +34,13 @@ router.delete("/delete/:id", async (req, res) => {
 router.put("/update/:id", async (req, res) => {
   const tUpdate = await Todo.updateOne(
     { _id: req.params.id },
-    //{$set:req.body}
+    { $set: req.body }
+    /*
     {
       author: "Bart",
       todo: "Skating",
     }
+    */
   );
 
   res.json(tUpdate);
